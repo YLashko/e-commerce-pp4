fb633435743321d6f46dce14386580fdf6877274
Wym <dewreator2@gmail.com>
2022-04-25 12:31:49 +0200

    Recover

---
+++ src/main/java/pl/ylashko/creditcard/CantAssignLimtiTwiceException.java
@@ -0,0 +1,4 @@
+package pl.ylashko.creditcard;
+
+public class CantAssignLimtiTwiceException extends IllegalStateException {
+}
---
+++ src/main/java/pl/ylashko/productcatalog/CantPublishProductException.java
@@ -0,0 +1,5 @@
+package pl.ylashko.productcatalog;
+
+public class CantPublishProductException
+        extends IllegalStateException {
+}
